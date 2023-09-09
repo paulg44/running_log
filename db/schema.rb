d448d98d079157e11d6b8ba88b18e1a60736f82a
paulg44 <paulgarton1984@hotmail.com>
2023-09-09 20:50:41 +0100

    Add form for sign in and link to sign up, add user model, currently non-functional

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_06_165216) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_09_194132) do
   create_table "log_fours", force: :cascade do |t|
     t.decimal "distance"
     t.decimal "duration"
@@ -59,6 +59,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_06_165216) do
     t.integer "pace"
     t.string "surface"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "password_confirmation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
